105af3dd418ccfc4a8b6ea8887e62a86ff976c0c
gopalbbharadva <gopalbbharadva@gmail.com>
2021-05-10 16:30:27 +0530

    setup:initial code setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import  Movie from './Components/Movie';
+
+const arr=['1','2','3'];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        arr.map(item  => {
+          return <Movie data={item} />
+        })
+      }
     </div>
   );
 }
---
+++ src/Components/Movie.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+const Movie=({data })=>{
+    return(
+        <div>{`item component ${data}`}</div>
+    )
+}
+
+export default Movie;
+
