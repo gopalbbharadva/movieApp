dbc728df91425cd4c54c3c0c653d86a315e05f68
gopalbbharadva <gopalbbharadva@gmail.com>
2021-05-10 18:14:02 +0530

    style:basic style applied

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import './App.css';
-import  Movie from './Components/Movie';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Movie from "./Components/Movie";
 
-const arr=['1','2','3'];
+const movie_api =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+
+const search_api =
+  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 function App() {
+
+  const [movies,setMovies]=useState([]);
+
+  useEffect(async ()=>{
+      await fetch(movie_api)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setMovies(data.results)
+      });
+  },[])
+
   return (
-    <div className="App">
-      {
-        arr.map(item  => {
-          return <Movie data={item} />
-        })
-      }
+    <div className="movie-container">
+      {movies.map((item) => {
+        return <Movie key={item.id} {...item} />;
+      })}
     </div>
   );
 }
--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 
-const Movie=({data })=>{
+
+const imagepath = "https://image.tmdb.org/t/p/w1280";
+
+const Movie=({title,overview,poster_path,vote_average})=>{
     return(
-        <div>{`item component ${data}`}</div>
+        <div className="movie">
+            <img src={imagepath+poster_path} alt={title}/>
+            <div className="movie-info">
+                <h3>{title}</h3>
+                <small>{vote_average}</small>
+            </div>
+        </div>
     )
 }
 
